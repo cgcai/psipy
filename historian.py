c5c734d347c8370aa18e880f4420b5a022979dc6
Camillus Gerard Cai <cgcai@qxcg.net>
2013-06-22 14:02:37 +0800

    Updated historian to use a JSON file format.

--- historian.py
+++ historian.py
@@ -3,6 +3,9 @@
 import argparse
 import json
 
+_LAST_TS_KEY = "last_ts"
+_HISTORIAN_FMT = "{timestamp} {value}\n"
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("scratch", help="historian scratch file")
@@ -10,11 +13,11 @@ def main():
 	parser.add_argument("history", help="file to write PSI history to")
 	args = parser.parse_args()
 
-	last_ts = read_scratch(args.scratch)
+	last_ts = get_last_ts(args.scratch)
 	psi = load_data(args.input)
 	last_ts, new_entries = process(psi, last_ts)
 	append_history(args.history, new_entries)
-	update_scratch(args.scratch, last_ts)
+	update_last_ts(args.scratch, last_ts)
 
 def process(data, last_ts):
 	base_date = int(data["date"])	
@@ -35,22 +38,34 @@ def load_data(path):
 	except IOError:
 		return None
 
+def get_last_ts(path):
+	scratch = read_scratch(path)
+	if scratch:
+		return scratch[_LAST_TS_KEY]
+	else:
+		return 0
+
+def update_last_ts(path, ts):
+	struct = {_LAST_TS_KEY: int(ts)}
+	write_scratch(path, struct)
+
 def read_scratch(path):
 	try:
 		with open(path, "r") as f:
 			contents = f.read()
-		return int(contents)
+		scratch_data = json.loads(contents)
+		return scratch_data
 	except IOError:
-		return 0
+		return None
 
-def update_scratch(path, ts):
+def write_scratch(path, json_data):
 	with open(path, "w") as f:
-		f.write(str(ts))
+		f.write(json.dumps(json_data))
 
 def append_history(path, new_entries):
 	with open(path, "a") as f:
 		for (ts, val) in new_entries:
-			f.write("{timestamp} {value}\n".format(timestamp=ts, value=val))
+			f.write(_HISTORIAN_FMT.format(timestamp=ts, value=val))
 
 if __name__ == '__main__':
 	main()
